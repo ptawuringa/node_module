a74771ceb3743e2f98ae370f726b49e3178685e5
ptawuringa <ptaruwinga@yahoo.com>
2020-08-04 12:05:56 +0200

    initial commit

---
+++ greet_xola.js
@@ -0,0 +1,10 @@
+//const greet = require('./greet');
+
+//console.log(greet('Xola'))
+
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
